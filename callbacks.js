b008aeca532a3d1720a0e855a76d7578fb85421f
syedibtisam <m.ibtisam.syed@gmail.com>
2024-02-18 16:11:09 +0000

    Promises all examples

--- callbacks.js
+++ callbacks.js
@@ -6,3 +6,13 @@ let promise = new Promise(function (resolve, reject) {
 promise.then(value => console.log(value + "-fulfilled"))
     .catch(error => console.log(error + "-error"))
     .finally(() => console.log("all done"));
+
+
+Promise.all(
+    [new Promise(resolve => resolve("first")),
+    new Promise(resolve => resolve("second")),
+    new Promise(resolve => resolve("third")),
+]
+).then(function(value){
+    console.log(value);
+})
